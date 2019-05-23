94f2ba5f8e7d364739495c4dd3d8d549e2a556df
Talha Hussain <talhahussainb@gmail.com>
2019-05-23 16:33:02 +0200

    Update MainSketch.ino

--- Arduino/MainSketch/MainSketch.ino
+++ Arduino/MainSketch/MainSketch.ino
@@ -137,95 +137,31 @@ void switchCases(String command){
 */
 
     if (cases.equals("FF") && sportM.equals("OF")){
-        speed=0;
-        while(speed<=50){
-          if(Serial1.available()>0){
-            break;
-            }
-            speed=speed+1;
-            car.setSpeed(speed);
-            delay(20);
-          }
+       
     } 
     
     if (cases.equals("BB") && sportM.equals("OF")){
-      speed=0;
-        while(speed >=-50){
-          if(Serial1.available()>0){
-            break;
-            }
-            speed=speed-1;
-            car.setSpeed(speed);
-            delay(20);
-          }
+     
       
     }
     
     if (cases.equals("LF")&& sportM.equals("OF")){
-      speed1=0;
-      speed2=0;
-      while(speed1 <= 50){
-          if(Serial1.available()>0){
-            break;
-            }
-            speed1=speed+1;
-            car.overrideMotorSpeed(speed1,speed2);
-            delay(20);
-          }
-      
-      
+ 
     }
     
     if (cases.equals("RF")&& sportM.equals("OF")){
-      speed1=0
-      speed2=0
-      while(speed2<=50){
-        if(Serial1.available()>0){
-          break;
-          }
-          speed2=speed2+1;
-          car.overrideMotorSpeed(speed1,speed2);
-          delay(20);
-          }
+     
     }
     
     if (cases.equals("LB")&& sportM.equals("OF")){
-      speed1=0;
-      speed2=0;
-      while(speed1 >= -50){
-          if(Serial1.available()>0){
-            break;
-            }
-            speed1=speed-1;
-            car.overrideMotorSpeed(speed1,speed2);
-            delay(20);
-          }
+     
     }
     
     if (cases.equals("RB")&& sportM.equals("OF")){
-      speed1=0
-      speed2=0
-      while(speed2 >= -50){
-        if(Serial1.available()>0){
-          break;
-          }
-          speed2=speed2-1;
-          car.overrideMotorSpeed(speed1,speed2);
-          delay(20);
-          }
       
     }
     
     if (cases.equals("RL")&& sportM.equals("OF")){
-      speed1=0;
-      speed2=0;
-      while(speed1>=-50 && speed2 <= 50){
-        speed1=speed1 - 1;
-        speed2=speed2 + 1;
-        car.overrideMotorSpeed(speed1,speed2);
-        delay(20);
-        
-        }
       
      
 
@@ -233,15 +169,7 @@ void switchCases(String command){
     }
 
     if (cases.equals("LR")&& sportM.equals("OF")){
-      speed1=0;
-      speed2=0;
-      while(speed1 <= 50 && speed2>=-50){
-        speed1=speed1+1;
-        speed2=speed2-1;
-        car.overrideMotorSpeed(speed1,speed2);
-        delay(20);
-        
-        }
+   
       
     }
     
